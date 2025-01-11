4640c0ae9a47a5075cd8e9883177511800d1451d
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2025-01-10 18:58:46 -0800

    merging choreo ctre test from fork to team repo (#3)
    transferring all choreo and ctre testing for mk4ns and autos from the beranki/FRC-2025 fork to the Tino-FRC-2473/FRC-2025 repo.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,15 +3,21 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
+
+import choreo.auto.AutoChooser;
+import choreo.auto.AutoFactory;
 // WPILib Imports
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.TunerConstants;
 // Systems
 import frc.robot.systems.DriveFSMSystem;
-import frc.robot.systems.Mech1FSMSystem;
-import frc.robot.systems.Mech2FSMSystem;
-import frc.robot.systems.AutoHandlerSystem;
-import frc.robot.systems.AutoHandlerSystem.AutoPath;
+// import frc.robot.systems.Mech1FSMSystem;
+// import frc.robot.systems.Mech2FSMSystem;
+// import frc.robot.systems.AutoHandlerSystem;
+// import frc.robot.systems.AutoHandlerSystem.AutoPath;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,13 +25,19 @@ import frc.robot.systems.AutoHandlerSystem.AutoPath;
  */
 public class Robot extends TimedRobot {
 	private TeleopInput input;
+	private TunerConstants constants;
 
 	// Systems
 	private DriveFSMSystem driveSystem;
-	private Mech1FSMSystem mech1System;
-	private Mech2FSMSystem mech2System;
+	private CommandSwerveDrivetrain swerveDrivetrain;
+	private AutoFactory autoFactory;
+	private AutoRoutines autoRoutines;
+	private AutoChooser autoChooser = new AutoChooser();
+	private Command autCommand;
+	// private Mech1FSMSystem mech1System;
+	// private Mech2FSMSystem mech2System;
 
-	private AutoHandlerSystem autoHandler;
+	// private AutoHandlerSystem autoHandler;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
@@ -36,45 +48,60 @@ public class Robot extends TimedRobot {
 		System.out.println("robotInit");
 		input = new TeleopInput();
 
+
 		// Instantiate all systems here
 		if (HardwareMap.isDriveHardwarePresent()) {
 			driveSystem = new DriveFSMSystem();
 		}
+		autoFactory = driveSystem.createAutoFactory();
+		autoRoutines = new AutoRoutines(autoFactory, driveSystem);
 
-		if (HardwareMap.isMech1HardwarePresent()) {
-			mech1System = new Mech1FSMSystem();
-		}
+		autoChooser.addRoutine("testPath", autoRoutines::testAuto);
+		SmartDashboard.putData("AUTO CHOOSER", autoChooser);
 
-		if (HardwareMap.isMech2HardwarePresent()) {
-			mech2System = new Mech2FSMSystem();
-		}
-		autoHandler = new AutoHandlerSystem(driveSystem, mech1System, mech2System);
+	// 	if (HardwareMap.isMech1HardwarePresent()) {
+	// 		mech1System = new Mech1FSMSystem();
+	// 	}
+
+	// 	if (HardwareMap.isMech2HardwarePresent()) {
+	// 		mech2System = new Mech2FSMSystem();
+	// 	}
+	// 	autoHandler = new AutoHandlerSystem(driveSystem, mech1System, mech2System);
+
+
 	}
 
 	@Override
 	public void autonomousInit() {
 		System.out.println("-------- Autonomous Init --------");
-		autoHandler.reset(AutoPath.PATH1);
+		// autoHandler.reset(AutoPath.PATH1);
+		autCommand = getAutonomousCommand();
+
+		if (autCommand != null) {
+			autCommand.schedule();
+		}
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-		autoHandler.update();
+		// autoHandler.update();
+		CommandScheduler.getInstance().run();
+		driveSystem.updateAutonomous();
 	}
 
 	@Override
 	public void teleopInit() {
 		System.out.println("-------- Teleop Init --------");
 		driveSystem.reset();
-		mech1System.reset();
-		mech2System.reset();
+		// mech1System.reset();
+		// mech2System.reset();
 	}
 
 	@Override
 	public void teleopPeriodic() {
 		driveSystem.update(input);
-		mech1System.update(input);
-		mech2System.update(input);
+		// mech1System.update(input);
+		// mech2System.update(input);
 	}
 
 	@Override
@@ -109,4 +136,13 @@ public class Robot extends TimedRobot {
 	// Do not use robotPeriodic. Use mode specific periodic methods instead.
 	@Override
 	public void robotPeriodic() { }
+
+	/**
+	 * Gets the autonomous command selected by the auto chooser.
+	 *
+	 * @return the selected autonomous command
+	 */
+	public Command getAutonomousCommand() {
+		return autoChooser.selectedCommand();
+	}
 }
