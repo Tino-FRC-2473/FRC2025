c5056bb5f5c2c5b87f56c9947fc17e033263b6b4
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2025-01-10 21:12:37 -0800

    fixed all checkstyles

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -13,4 +13,6 @@ public class DriveConstants {
 
 	public static final double DRIVE_P = 0.1;
 	public static final double DRIVE_V = 0.124;
+
+	public static final double STEER_CURRENT_LIMIT = 60;
 }
