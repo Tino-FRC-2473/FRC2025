bed02848560aa355d307e77ae7f05fb4d961f0fa
zekr0m <107783785+zekr0m@users.noreply.github.com>
2025-01-16 17:42:04 -0800

    add selection based on USB id for driver cam

--- src/main/cv/python/StreamDriverCam.py
+++ src/main/cv/python/StreamDriverCam.py
@@ -2,12 +2,21 @@ import cv2
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socket
+from linuxpy.video.device import Device
 
 PORT = 1181
-SERVER_IP = socket.gethostbyname(socket.gethostname())
-CAM_INDEX = 1
+LISTEN_IP =  '0.0.0.0' # 0.0.0.0 means accessible from any ip
+CAM_USB_ID = "usb-xhci-hcd.0-2" # based on port of raspberry pi (top right)
 
-camera = cv2.VideoCapture(CAM_INDEX)
+def find_camera_index(usbId):
+    index = 0
+    while index < 4: # there can't be more than 4 cameras, I think
+        with Device.from_id(index) as cam:
+            if cam.info.bus_info == usbId:
+                return index
+
+index = find_camera_index(CAM_USB_ID)
+camera = cv2.VideoCapture(index)
 class MJPEGStreamHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/stream.mjpg':
@@ -38,9 +47,9 @@ class MJPEGStreamHandler(BaseHTTPRequestHandler):
 
 
 def StreamDriverCam():
-    server_address = (SERVER_IP, PORT)
+    server_address = (LISTEN_IP, PORT) 
     httpd = HTTPServer(server_address, MJPEGStreamHandler)
-    print(f'Streaming video at http://{SERVER_IP}:{PORT}/stream.mjpg')
+    print(f'Streaming video at http://{LISTEN_IP}:{PORT}/stream.mjpg')
     httpd.serve_forever()
 
 StreamDriverCam()
