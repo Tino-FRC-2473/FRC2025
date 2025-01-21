000a97eadab3070ef9e1d771248db0ce5e9fdc28
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2025-01-20 19:19:54 -0800

    add new TunerConstants

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -10,7 +10,6 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import choreo.trajectory.SwerveSample;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -45,12 +44,6 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain<TalonFX, TalonFX, 
 	private final PIDController autoXPid = new PIDController(5, 0, 0);
 	private final PIDController autoYPid = new PIDController(5, 0, 0);
 	private final PIDController autoHeadingPid = new PIDController(0.75, 0, 0);
-
-	private final SwerveDrivePoseEstimator swerveDrivePoseEstimator =
-		new SwerveDrivePoseEstimator(
-			getKinematics(),
-			getPigeon2().getRotation2d(),
-			getModulePositions(), new Pose2d());
 
 	private MapleSimSwerveDrivetrain mapleSimSwerveDrivetrain;
 	private Notifier simNotifier;
