831461bd10b65ec80bc0c10dea7d602e037804ab
Kavya <92774828+KGupta10@users.noreply.github.com>
2025-01-22 21:59:12 -0800

    fix two camera bugs

--- src/main/cv/python/visionInput.py
+++ src/main/cv/python/visionInput.py
@@ -4,9 +4,12 @@ def find_camera_index(usbId):
     from linuxpy.video.device import Device
     
     for index in range(4): # there probably won't be more than 4 cameras on the pi
-        with Device.from_id(index) as cam:
-            if cam.info.bus_info == usbId:
-                return index
+        try:
+            with Device.from_id(index) as cam:
+                if cam.info.bus_info == usbId:
+                    return index
+        except:
+            print("not connected")
             
     print(f"find_camera_index: Could not find camera on {usbId}")
 
