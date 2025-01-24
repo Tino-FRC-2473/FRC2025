62e9561f58d237f5dd960da9ea67e2eeafcbb2a5
oliver <116319227+queerboi@users.noreply.github.com>
2025-01-23 18:06:04 -0800

    remove climber motion magic constants

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -6,8 +6,8 @@ package frc.robot;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-	public static final int CAN_ID_ELEVATOR = 5; // TBD, was 8 in testing
-	public static final int CAN_ID_CLIMBER = 8; // TBD
+	public static final int CAN_ID_ELEVATOR = 9; // TBD, was 8 in testing
+	public static final int CAN_ID_CLIMBER = 12; // for testing
 
 	// playing with fusion CAN chain
 	public static final int FUNNEL_TOF_ID = 13; // TBD
@@ -18,7 +18,7 @@ public final class HardwareMap {
 
 	public static final int FUNNEL_BREAK_BEAM_ID = 3; // TBD
 
-	public static final int CLIMBER_LIMIT_SWITCH_PORT = 4; // TBD
+	public static final int CLIMBER_LIMIT_SWITCH_PORT = 0; // for testing only
 
 	// rio - pwm (servo) ports
 	public static final int FUNNEL_SERVO_PORT = 9; // not confirmed with hardware
--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -50,11 +50,6 @@ public final class Constants {
 	public static final double ELEVATOR_CONFIG_CONSTANT_A = 80; // Max acceleration in rps/s (80)
 	public static final double ELEVATOR_CONFIG_CONSTANT_J = 110; // Target jerk in rps/s/s (110)
 
-	// to be tuned for climber
-	public static final double CLIMBER_CONFIG_CONSTANT_CV = 14; // Cruise Velo in rps (10)
-	public static final double CLIMBER_CONFIG_CONSTANT_A = 80; // Max acceleration in rps/s (80)
-	public static final double CLIMBER_CONFIG_CONSTANT_J = 110; // Target jerk in rps/s/s (110)
-
 	// Other
 	public static final int UPDATE_FREQUENCY_HZ = 100;
 		// this is the lowest possible value since we refresh ourselves
