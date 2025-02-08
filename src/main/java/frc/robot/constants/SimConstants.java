8267ab6e9bc8b389e3705855aeefa57701d0caa6
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-02-08 12:16:43 -0800

    (fix) updated bumper constants

--- src/main/java/frc/robot/constants/SimConstants.java
+++ src/main/java/frc/robot/constants/SimConstants.java
@@ -15,8 +15,8 @@ public class SimConstants {
 
 	// Estimated values for now, need to be calculated later
 	public static final double MASS_WITH_BUMPER_LBS = 115;
-	public static final double WIDTH_IN = 30;
-	public static final double LENGTH_IN = 30;
+	public static final double WIDTH_IN = 35.5;
+	public static final double LENGTH_IN = 35.5;
 	public static final double WHEEL_COF = 1.2;
 
 	// mech pose logging constants
