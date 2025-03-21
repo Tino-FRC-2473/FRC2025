fix station tag pose only correct using rotation matrix

src/main/cv/python/apriltag.py
@@ -161,8 +161,10 @@ class AprilTag():
         gray = image[:, :, 0]
         results = self.detector.detect(gray)
         pose_list = []
+
         # print("results from distance to tag", results)
         corners = [r.corners for r in results]
+
         if(len(corners) > 0):
             corners = [r.corners for r in results]
             # Testing with coral station apriltag
@@ -174,46 +176,42 @@ class AprilTag():
             _, rvec, tvec = cv2.solvePnP(marker_points_3d, image_points_2d, self.camera_matrix, self.dist_coeffs)
 
             # transform rvec: Vtr = Rx(Vtc) + Vcr
-            angle = 10 # arbitrary
+            angle = 0.331613 # 19 degree tilt converted to radians
             R_x = np.array([
                 [1, 0, 0],
                 [0, np.cos(angle), -np.sin(angle)],
                 [0, np.sin(angle), np.cos(angle)]
             ])
+
             rot_transform = (R_x @ rvec) # returns a 1 by 3 'matrix'
 
-            #print("first value: ", rot_transform[1][0])
+            print("first value: ", rot_transform[1][0])
+            #stores the pose in a np array
             rot_transform_parsed = np.array([rot_transform[0][0], rot_transform[1][0], rot_transform[2][0]])
-            cam_robot_rot_euler = np.array([0, 0.332, 0]) # euler angle --> rotation vector
-            print("Shape of cam-robot vec: ", cam_robot_rot_euler.shape)
-            tag_robot_rotation = rot_transform_parsed + cam_robot_rot_euler
-            print("tag robot rotation value: ", tag_robot_rotation)
+            print(rot_transform_parsed)
+            # cam_robot_rot_euler = np.array([0, 0.332, 0]) # euler angle --> rotation vector
+            # print("Shape of cam-robot vec: ", cam_robot_rot_euler.shape)
+            # tag_robot_rotation = rot_transform_parsed + cam_robot_rot_euler
+            # print("tag robot rotation value: ", tag_robot_rotation)
 
             # transform tvec: Vtr = Rx(Vtc) + Vcr
+
+            #multiply the translation pose by Rx(19)
             trans_transform = (R_x @ tvec)
             trans_transform_parsed = np.array([trans_transform[0][0], trans_transform[1][0], trans_transform[2][0]])
-            trans_shift = [-0.130175, 0.903224, 0.0536]
-            cam_robot_trans_euler = np.array(trans_shift)
-            tag_robot_trans = trans_transform_parsed + cam_robot_trans_euler # 1 by 3 'matrix'
+            print(trans_transform_parsed)
+
+            # trans_shift = [-0.130175, 0.903224, 0.0536]
+            # cam_robot_trans_euler = np.array(trans_shift)
+            # tag_robot_trans = trans_transform_parsed + cam_robot_trans_euler # 1 by 3 'matrix'
 
             #cvec = np.linalg.inv(tag_robot_trans)
-            pose_list = [tag_robot_trans, tag_robot_rotation]
+            pose_list = [trans_transform_parsed, rot_transform_parsed]
 
 
         return pose_list
     
-    def correct_station_angle(self, rvec):
-        R, _ = cv2.Rodrigues(rvec)
-        # there's a negative for the x position b/c to the left is negative in the opencv2 systems
-        list = [0, 0.331613,0]
-
-        intake_rvec = np.array(list)
-        pose_list = R.T @ (rvec - intake_rvec)
-        # multiplying by negative one b/c of the way that vector adition works
-        pose_list[2] = -1 * pose_list[2]
-        pose_list[0] = -1 * pose_list[0]
-
-        return pose_list
+   
 
     def calibrate_camera(RES: tuple[int, int], input_dir_relative: Path, output_dir_relative: Path, square_size: int, width: int, height: int, file_name: str, bw_camera: bool, visualize=False):
         """