5c0397a26fba89da0249187f02f83fc0996d3250
Bhargav Eranki <bhargav.eranki@gmail.com>
2025-03-30 10:10:40 -0700

    revert alignment changes

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -49,7 +49,7 @@ public final class Constants {
 
 	public static final double CLIMBER_PID_TARGET_LOW = 0;
 	public static final double CLIMBER_PID_TARGET_EXTEND = 72.22;
-	public static final double CLIMBER_PID_TARGET_CLIMB = 180;
+	public static final double CLIMBER_PID_TARGET_CLIMB = 187;
 	public static final double CLIMBER_ENCODER_RESET_POSITION = CLIMBER_COUNTS_PER_REV;
 
 	public static final double CLIMB_POWER = 1.0;
